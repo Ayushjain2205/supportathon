03331c51e76217bdf91c3d453833364560d0f131
Ayush Jain <ayushjain2205@gmail.com>
2022-12-18 04:30:02 +0530

    Add conditional rendering for landing page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 import { logo, sun } from "../assets";
 import { navlinks } from "../constants";
@@ -29,33 +29,38 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
 
-  return (
-    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-      <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#ffffff]" imgUrl={logo} />
-      </Link>
+  let location = useLocation();
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#ffffff] rounded-[20px] w-[76px] py-4 mt-12">
-        <div className="flex flex-col justify-center items-center gap-3">
-          {navlinks.map((link) => (
-            <Icon
-              key={link.name}
-              {...link}
-              isActive={isActive}
-              handleClick={() => {
-                if (!link.disabled) {
-                  setIsActive(link.name);
-                  navigate(link.link);
-                }
-              }}
-            />
-          ))}
+  if (location.pathname == "/landing") {
+    return <div></div>;
+  } else
+    return (
+      <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+        <Link to="/">
+          <Icon styles="w-[52px] h-[52px] bg-[#ffffff]" imgUrl={logo} />
+        </Link>
+
+        <div className="flex-1 flex flex-col justify-between items-center bg-[#ffffff] rounded-[20px] w-[76px] py-4 mt-12">
+          <div className="flex flex-col justify-center items-center gap-3">
+            {navlinks.map((link) => (
+              <Icon
+                key={link.name}
+                {...link}
+                isActive={isActive}
+                handleClick={() => {
+                  if (!link.disabled) {
+                    setIsActive(link.name);
+                    navigate(link.link);
+                  }
+                }}
+              />
+            ))}
+          </div>
+
+          <Icon styles="bg-[#c8e4f9] shadow-secondary" imgUrl={sun} />
         </div>
-
-        <Icon styles="bg-[#c8e4f9] shadow-secondary" imgUrl={sun} />
       </div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar;
